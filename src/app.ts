484fe2ee19861feab9989049bdd9b8a8716de384
Amaraporn07 <amaraporn.onkhoksung@gmail.com>
2026-01-17 12:30:10 +0700

    .

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,13 @@
 import express from 'express';
 import { initDb } from './db/index.js';
 
-import customerRoutes from './routes/customer.route.js';
-import productRoutes from './routes/product.route.js';
-import categoryRoutes from './routes/category.route.js';
-import supplierRoutes from './routes/supplier.route.js';
-import orderRoutes from './routes/order.route.js';
+import productRoutes from './product/index.js';
 
 const app = express();
 app.use(express.json());
 
 initDb();
 
-app.use('/api/customers', customerRoutes);
 app.use('/api/products', productRoutes);
-app.use('/api/categories', categoryRoutes);
-app.use('/api/suppliers', supplierRoutes);
-app.use('/api/orders', orderRoutes);
 
 export default app;
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,43 +1,15 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
-// เปิด connection (ใช้ร่วมกันทั้งโปรเจกต์)
 export const dbPromise = open({
   filename: 'app.db',
   driver: sqlite3.Database,
 });
 
-// init database + schema
 export const initDb = async () => {
   const db = await dbPromise;
 
   await db.exec(`
-    CREATE TABLE IF NOT EXISTS Customer (
-      CustomerID INTEGER PRIMARY KEY AUTOINCREMENT,
-      Name TEXT,
-      Email TEXT,
-      Phone TEXT,
-      Address TEXT,
-      RegisterDate DATETIME DEFAULT CURRENT_TIMESTAMP
-    );
-
-    CREATE TABLE IF NOT EXISTS Category (
-      CategoryID INTEGER PRIMARY KEY AUTOINCREMENT,
-      Name TEXT,
-      Description TEXT,
-      Status TEXT,
-      CreatedDate DATETIME DEFAULT CURRENT_TIMESTAMP
-    );
-
-    CREATE TABLE IF NOT EXISTS Supplier (
-      SupplierID INTEGER PRIMARY KEY AUTOINCREMENT,
-      Name TEXT,
-      Contact TEXT,
-      Address TEXT,
-      Email TEXT,
-      Phone TEXT
-    );
-
     CREATE TABLE IF NOT EXISTS Product (
       ProductID TEXT PRIMARY KEY,
       Name TEXT,
@@ -45,15 +17,6 @@ export const initDb = async () => {
       Stock INTEGER,
       Description TEXT,
       CategoryID INTEGER
-    );
-
-    CREATE TABLE IF NOT EXISTS Orders (
-      OrderID INTEGER PRIMARY KEY AUTOINCREMENT,
-      OrderDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-      TotalAmount REAL,
-      Status TEXT,
-      PaymentMethod TEXT,
-      CustomerID INTEGER
     );
   `);
 
